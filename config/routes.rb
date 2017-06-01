6154030e0ed99336be34c11605b6f804696688e7
Miguelwm <miguelpereira.wm@gmail.com>
2017-06-01 18:33:29 +0100

    Static2

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,3 @@
-##Rails.application.routes.draw do
-
-  get 'foo/bar'
-
-  get 'foo/baz'
-
-## get 'static_pages/...'
-
-##  get 'static_pages/home'
-
-##  get 'static_pages/help'
-
-##  root 'application#hello'
-
-##end
 Rails.application.routes.draw do
   get  'static_pages/home'
   get  'static_pages/help'
