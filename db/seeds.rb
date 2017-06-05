5222ed819c98a810488f12d8f72c2af633a1162f
Miguelwm <miguelpereira.wm@gmail.com>
2017-06-05 14:26:41 +0100

    Finish user edit, update, index, and destroy actions

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!(name:  "teste1",
+             email: "teste1@exemplo.com",
+             password:              "teste1",
+             password_confirmation: "teste1",
+             admin: true)
+
+99.times do |n|
+  name  = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  password = "password"
+  User.create!(name:  name,
+               email: email,
+               password:              password,
+               password_confirmation: password)
+end
